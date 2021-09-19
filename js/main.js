e0f68f6c105bfc8326ac6f8c2f2327595a5cba51
Vlad Kravchenko <57967177+VladKravchenko05082000@users.noreply.github.com>
2021-09-19 21:38:00 +0300

    Update 19.09.2021 - header-done adaptive header-done. Main block - almost complete Main block adaptive - none

---
+++ js/main.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".header__burger").click(function(e){$(".header__burger, .header__menu").toggleClass("active")}),$(".header__menu").click(function(e){$(".header__burger, .header__menu").removeClass("active")})});
