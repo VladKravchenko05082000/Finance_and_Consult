7653aa32acfd9a5f16d6787bd3656c1aeb7502df
Vlad Kravchenko <57967177+VladKravchenko05082000@users.noreply.github.com>
2021-09-23 00:07:47 +0300

    New update 22.09.2021 Services - done adaptive Services - done Experience - done adaptive Experience - done Practice - done adaptive Practice - done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".header__burger").click(function(e){$(".header__burger, .header__menu").toggleClass("active")}),$(".header__menu").click(function(e){$(".header__burger, .header__menu").removeClass("active")})});
+$(document).ready(function(){$(".header__burger").click(function(e){$(".header__burger, .header__menu").toggleClass("active"),$("body").toggleClass("lock")}),$(".header__menu").click(function(e){$(".header__burger, .header__menu").removeClass("active")})});
